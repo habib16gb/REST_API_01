f1a84262debaad51daf66cdd5eaa37b02a660041
habib16gb <habib16gb@gmail.com>
2023-07-13 19:30:58 +0100

    etup Express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hello from typescript!!')
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(port, () =>
+  console.log(`server running on http://localhost:${port}/`)
+);
