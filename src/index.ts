615cef482838a0e7a5ea3ce98f15256a210de466
habib16gb <habib16gb@gmail.com>
2023-07-13 20:02:25 +0100

    MongoDB Setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
@@ -23,3 +24,10 @@ const server = http.createServer(app);
 server.listen(port, () =>
   console.log(`server running on http://localhost:${port}/`)
 );
+
+const MONGO_URL =
+  "mongodb+srv://habib16gb:<ep1FLCuQZmtTsBn0>@cluster0.uq1kl86.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.error(error));
